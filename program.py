b29e8b4b4509bb3204723ca532011bf889457d64
valerius <valerius>
2020-04-22 21:24:39 +0200

    working conversion to x and y coordinates

--- program.py
+++ program.py
@@ -28,18 +28,22 @@ with open('file.ubh') as recording:
             i+=1
 
     lidar_distances = np.array(end_result).reshape((i,1081))
+    xy_values = []
     k = 0
     for item in lidar_distances:
         j = 0
+        row = []
         for thing in item:
             if thing != 60000:
                 if j < 540:
-                    value = (thing*m.sin(j*0.25-45), thing*m.cos(j*0.25-45))
+                    value = (thing*m.cos(m.radians(j*0.25-45)), thing*m.sin(m.radians(j*0.25-45)))
                 elif j == 540:
                     value = (0,thing)
                 else:
-                    value = (thing*m.sin(j*0.25-225), -thing*m.cos(j*0.25-225))
+                    value = (-thing*m.cos(m.radians(225-j*0.25)), thing*m.sin(m.radians(225-j*0.25)))
+                row.append(value)
             j+=1
+        xy_values.append(row)
         k+=1
 
     
